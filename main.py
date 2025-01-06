be8ff615d78ca779c8f236abc3b4bb5d1a8664d1
mgalindo <mgalindodlt@gmail.com>
2025-01-06 15:40:21 -0800

    Added rotation

--- main.py
+++ main.py
@@ -3,20 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 # import everything from a module
 # into the current file
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player1=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000)
+            
+        player1.update(dt)    
+
+        screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
